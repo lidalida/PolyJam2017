25b4a9d976cf816f9b81b0d92396b8233fb469de
Vonski <daniellodelapedro@interia.pl>
2017-01-21 08:22:27 +0100

    Working Morse Transmission, first preview scene

---
+++ PolyJam2017/Assets/ShipController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipController : MonoBehaviour {
+
+    public Sprite n, ne, e, se, s, sw, w, nw;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
